93811b2d86cedebe05573222a51ce4ea8a0517d5
darkshinra154-lgtm <darkshinra154@gmail.com>
2026-08-29 19:21:07 +0300

    Delete models directory nm

--- models/SubBotSession.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-const SubBotSchema=new mongoose.Schema({
-jid:{type:String,required:true,unique:true},
-creds:{type:Object,required:true},
-owner:{type:String,default:'Unknown'},
-createdAt:{type:Date,default:Date.now}
-});
-export default mongoose.model('SubBotSession',SubBotSchema);
